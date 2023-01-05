3e629fb1ce1ccf95962670d649e4dbdda5cdfab2
jungHyeonS <fbtls25@naver.com>
2023-01-05 23:11:30 +0900

    feat : apolloProvider

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,6 +1,34 @@
-import React from "react"
+import { gql, useApolloClient } from "@apollo/client"
+import React, { useEffect, useState } from "react"
+
+interface Movie {
+  id: number
+  title: string
+  __typename: string
+}
+
 
 const Movies = () => {
-    return <div>Movies</div>
+  const [movies,setMovies] = useState<Movie[]>([]);
+  const client = useApolloClient()
+  useEffect(()=>{
+      client.query({
+          query:gql`
+              {
+                  allMovies{
+                      title,
+                      id
+                  }
+              }
+          `
+      }).then(results => setMovies(results.data.allMovies))
+  },[client])
+  return (
+    <ul>
+      {movies.map((movie : Movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  )
 }
 export default Movies
