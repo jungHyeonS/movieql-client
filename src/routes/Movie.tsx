276ba629f2baa7f944f319f7af8a63f097724f84
jungHyeonS <fbtls25@naver.com>
2023-01-08 16:46:24 +0900

    feat : movie style change

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,27 +1,68 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($movieId:String!){
     movie(id:$movieId){
       id
       title
+      medium_cover_image
+      rating
     }
   }
 `
 
 interface Movie{
   id:number,
-  title:string
+  title:string,
+  medium_cover_image?:string,
+  rating:number
 }
 
 interface GetMovieProps{
   movie : Movie
 }
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
 
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
 
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div<{bg:string}>`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
 
 const Movie = () => {
   const {id} = useParams();
@@ -33,9 +74,14 @@ const Movie = () => {
 
   
 
-  if(loading){
-    return <h1>Fetching Movie...</h1>
-  }
-  return <h1>{data?.movie.title}</h1>
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data?.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image || ''} />
+    </Container>
+  )
 }
 export default Movie;
