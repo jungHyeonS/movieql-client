6192024b28216d0413809835002cb5a28c18bd34
jungHyeonS <fbtls25@naver.com>
2023-01-08 16:32:32 +0900

    feat : apollo useQuery

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,6 +1,41 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId:String!){
+    movie(id:$movieId){
+      id
+      title
+    }
+  }
+`
+
+interface Movie{
+  id:number,
+  title:string
+}
+
+interface GetMovieProps{
+  movie : Movie
+}
+
+
+
 
 const Movie = () => {
-    return <div>This is Movie detail</div>
+  const {id} = useParams();
+  const {data,loading} = useQuery<GetMovieProps>(GET_MOVIE,{
+    variables:{
+      movieId:id
+    }
+  })
+
+  
+
+  if(loading){
+    return <h1>Fetching Movie...</h1>
+  }
+  return <h1>{data?.movie.title}</h1>
 }
 export default Movie;
--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,5 +1,6 @@
-import { gql, useApolloClient } from "@apollo/client"
+import { gql, useApolloClient, useQuery } from "@apollo/client"
 import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 interface Movie {
   id: number
@@ -8,26 +9,61 @@ interface Movie {
 }
 
 
+interface author {
+  fullName:string
+}
+
+interface Tweet{
+  id: number
+  text: string
+  __typename: string
+  author?: author
+}
+
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies{
+        title,
+        id
+    }
+    allTweets{
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`
+
 const Movies = () => {
-  const [movies,setMovies] = useState<Movie[]>([]);
-  const client = useApolloClient()
-  useEffect(()=>{
-      client.query({
-          query:gql`
-              {
-                  allMovies{
-                      title,
-                      id
-                  }
-              }
-          `
-      }).then(results => setMovies(results.data.allMovies))
-  },[client])
+  const { data, loading, error} = useQuery(ALL_MOVIES)
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+
+  if(error){
+    return <h1>Coud not fetch</h1>
+  }
+
   return (
     <ul>
-      {movies.map((movie : Movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
+      <h1>Movies</h1>
+      {data.allMovies.map((movie : Movie) => 
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>
+            {movie.title}
+          </Link>
+          
+        </li>  
+      )}
+      <h1>Tweets</h1>
+      {data.allTweets.map((tweet : Tweet) => 
+        <li key={tweet.id}>
+          {tweet.text}/ by : {tweet.author?.fullName}
+        </li>  
+      )}
     </ul>
   )
 }
